e6109ae984ff96fa7d42c0139a8c15a3b94458d0
Thaweewat <thaweewat.data@gmail.com>
2023-08-10 08:32:38 +0700

    test

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_sensor_log():
     table = client.query(query=query, database="sensor_ytp", language="sql")
     # Convert to dataframe
     df = table.to_pandas().sort_values(by="time")
-    select_col = ['humidity', 'temperature', 'ldrValue', 'pirValue', 'time', 'rssi', 'soilValue']
+    select_col = ['humidity', 'temperature', 'ldrValue', 'pirValue', 'time', 'rssi', 'soilValue', 'waterTemperature']
     # resample time to 1 sec
     df['time'] = pd.to_datetime(df['time'])
     df = df.groupby(pd.Grouper(key='time', freq='1s')).mean(numeric_only=True).reset_index().bfill().ffill()
@@ -48,33 +48,39 @@ def get_motor_log():
 
 
 df_raw = get_sensor_log()
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5 = st.columns(5)
 # get avg temp and change in pass 5 mins
 st.title("Eureka Dashboard")
 
-# Calculate the average temperature and round it
-avg_temperature = round(df_raw.set_index('time')['temperature'].mean())
 
-# Assuming 'temperature' data is at a minute level
-last_minute_temperature = df_raw.set_index('time').last('1T')['temperature']
+def get_percent_diff(df_raw, target_col):
+    # Calculate the average temperature and round it
+    avg_temperature = round(df_raw.set_index('time')[target_col].mean())
 
-# Calculate the percentage difference
-if len(last_minute_temperature) > 1:
-    percent_diff = ((last_minute_temperature[-1] - last_minute_temperature[0]) / last_minute_temperature[0]) * 100
-else: # You can't compute % diff if there is no change in time
-    percent_diff = np.nan
+    # Assuming 'temperature' data is at a minute level
+    last_minute_temperature = df_raw.set_index('time').last('1T')[target_col]
 
-# RuntimeError handling: If you try to compute % diff when last_minute_temperature[0] = 0
-try:
-    percent_diff = ((last_minute_temperature[-1] - last_minute_temperature[0]) / last_minute_temperature[0]) * 100
-except ZeroDivisionError:
-    percent_diff = np.nan
-    # handle the special case as you need
+    # Calculate the percentage difference
+    if len(last_minute_temperature) > 1:
+        percent_diff = ((last_minute_temperature[-1] - last_minute_temperature[0]) / last_minute_temperature[0]) * 100
+    else: # You can't compute % diff if there is no change in time
+        percent_diff = np.nan
+
+    # RuntimeError handling: If you try to compute % diff when last_minute_temperature[0] = 0
+    try:
+        percent_diff = ((last_minute_temperature[-1] - last_minute_temperature[0]) / last_minute_temperature[0]) * 100
+    except ZeroDivisionError:
+        percent_diff = np.nan
+
+    return percent_diff
+
 
 # Assume col1 is a Streamlit object that uses the metric method to display metrics on an app
-col1.metric("Avg Temperature", round(df_raw['temperature'].mean()), percent_diff)
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1.metric("Avg Temperature", round(df_raw['temperature'].mean()), round(get_percent_diff(df_raw, 'temperature'), 2) + "%")
+col2.metric("Humidity", round(df_raw['humidity'].mean()), round(get_percent_diff(df_raw, 'humidity'), 2) + "%")
+col3.metric("Soil Moisture", round(df_raw['soilValue'].mean()), round(get_percent_diff(df_raw, 'soilValue'), 2) + "%")
+col4.metric("LDR", round(df_raw['ldrValue'].mean()), round(get_percent_diff(df_raw, 'ldrValue'), 2) + "%")
+col5.metric("Water Temperature", round(df_raw['waterTemperature'].mean()), round(get_percent_diff(df_raw, 'waterTemperature'), 2) + "%")
 st.dataframe(get_motor_log().head(10))
 st.subheader(get_today_str())
 
