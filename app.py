18b25e58cb91ded8d53910f2425e15419b464bad
Thaweewat <thaweewat.data@gmail.com>
2023-08-10 07:49:32 +0700

    test

--- app.py
+++ app.py
@@ -6,25 +6,17 @@ from streamlit_image_coordinates import streamlit_image_coordinates
 
 
 def get_today_str():
-	now = time.localtime()
-	# YYYY-MM-DD HH-MM
-	return f"{now.tm_year}-{now.tm_mon}-{now.tm_mday} {now.tm_hour}-{now.tm_min}"
+    now = time.localtime()
+    # YYYY-MM-DD HH-MM
+    return f"{now.tm_year}-{now.tm_mon}-{now.tm_mday} {now.tm_hour}-{now.tm_min}"
 
 
 def get_dataset():
-    token = os.environ.get(
-        "OZpKr76YbuNbXqIzPSkVeLTkghXJuX7oZJiy4iBjDfEYrwD6swbSW7gQbH4qQMAq-oD9lQEwTD3CRGZzt3ekDQ=="
-    )
+    token = "OZpKr76YbuNbXqIzPSkVeLTkghXJuX7oZJiy4iBjDfEYrwD6swbSW7gQbH4qQMAq-oD9lQEwTD3CRGZzt3ekDQ=="
     org = "ytp"
     host = "https://us-east-1-1.aws.cloud2.influxdata.com"
     client = InfluxDBClient3(host=host, token=token, org=org)
-
-    query = """SELECT *
-	FROM "sensor_port"
-	WHERE
-	time >= now() - interval '1 day'"""
-
-    # Execute the query
+    query = """SELECT * FROM "sensor_port" WHERE time >= now() - interval '1 day'"""
     table = client.query(query=query, database="sensor_processed", language="influxql")
     # Convert to dataframe
     df = table.to_pandas().sort_values(by="time")
